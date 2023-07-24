b63432ccc3d5949aa362af66d082fb3366a9ffe2
libjared <libjared@users.noreply.github.com>
2023-07-23 23:12:23 -0500

    Prevent observer infinite feedback loop

--- src/content.js
+++ src/content.js
@@ -20,40 +20,48 @@ if (isMostLikelyMastodon) {
 				$choiceBox = $profileUrlInput.closest('.interaction-modal__choices__choice');
 				if (!$choiceBox) return;
 
-				chrome.storage.sync.get(
-					{
-						local_domain: '',
-						web_domain: '',
-					},
-					function (items) {
-						const LOCAL_DOMAIN = items.local_domain;
-						const WEB_DOMAIN = items.web_domain || LOCAL_DOMAIN;
+				if ($choiceBox.dataset.redirectModificationsState !== 'done') {
+					const $existingParagraph = $choiceBox.querySelector('p');
+					const $existingHeader = $choiceBox.querySelector('h3 span');
 
-						// Not configured? Show a notification.
-						if (!WEB_DOMAIN) {
-							$choiceBox.querySelector('p').innerText = 'Please configure the mastodon-profile-redirect browser extension to more easily follow this account, directly on your Mastodon instance.';
-							return;
+					chrome.storage.sync.get(
+						{
+							local_domain: '',
+							web_domain: '',
+						},
+						function (items) {
+							const LOCAL_DOMAIN = items.local_domain;
+							const WEB_DOMAIN = items.web_domain || LOCAL_DOMAIN;
+
+							// Not configured? Show a notification.
+							if (!WEB_DOMAIN && $choiceBox.dataset.redirectModificationsState !== 'unconfigured') {
+								$existingParagraph.innerText = 'Please configure the mastodon-profile-redirect browser extension to more easily follow this account, directly on your Mastodon instance.';
+								$choiceBox.dataset.redirectModificationsState = 'unconfigured';
+								return;
+							}
+
+							// Trim off @domain suffix in case it matches with LOCAL_DOMAIN. This due to https://github.com/mastodon/mastodon/issues/21469
+							if (user.endsWith(`@${LOCAL_DOMAIN}`)) {
+								user = user.substring(0, user.length - `@${LOCAL_DOMAIN}`.length);
+							}
+
+							// Change title to reflect user’s Masto instance
+							$existingHeader.innerText = `On ${LOCAL_DOMAIN}`;
+
+							// Create view profile button
+							const $viewButton = document.createElement('a');
+							$viewButton.classList.add('button', 'button--block');
+							$viewButton.href = `https://${WEB_DOMAIN}/@${user}`;
+							$viewButton.innerText = 'View Profile';
+
+							// Replace the orig paragraph with the show profile button
+							$existingParagraph.insertAdjacentElement('beforebegin', $viewButton);
+							$choiceBox.removeChild($existingParagraph);
+
+							$choiceBox.dataset.redirectModificationsState = 'done';
 						}
-
-						// Change title to reflect user’s Masto instance
-						$choiceBox.querySelector('h3 span').innerText = `On ${LOCAL_DOMAIN}`;
-
-						// Trim off @domain suffix in case it matches with LOCAL_DOMAIN. This due to https://github.com/mastodon/mastodon/issues/21469
-						if (user.endsWith(`@${LOCAL_DOMAIN}`)) {
-							user = user.substring(0, user.length - `@${LOCAL_DOMAIN}`.length);
-						}
-
-						// Create view profile button
-						const $viewButton = document.createElement('a');
-						$viewButton.classList.add('button', 'button--block');
-						$viewButton.href = `https://${WEB_DOMAIN}/@${user}`;
-						$viewButton.innerText = 'View Profile';
-
-						// Replace the orig paragraph with the show profile button
-						$choiceBox.querySelector('p').insertAdjacentElement('beforebegin', $viewButton);
-						$choiceBox.removeChild($choiceBox.querySelector('p'));
-					}
-				);
+					);
+				}
 			});
 		});
 
