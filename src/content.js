6c89cf8eee1ad98b3ad1909a60ec1075dfaebc2e
libjared <libjared@users.noreply.github.com>
2023-08-06 15:14:06 -0500

    Try to handle glitch-soc more

--- src/content.js
+++ src/content.js
@@ -1,33 +1,48 @@
 const $mastodon = document.querySelector('#mastodon');
+let LOCAL_DOMAIN = '';
+let WEB_DOMAIN = '';
 
 if ($mastodon) {
 	console.log('Mastodon Redirector is running.');
 
-	const $initialModalRoot = $mastodon.querySelector('.modal-root');
-	if ($initialModalRoot) {
-		foundModalRoot($initialModalRoot);
-	} else {
-		const observerModalRoot = new MutationObserver(function (mutations_list) {
-			mutations_list.forEach(function (mutation) {
-				if (!mutation.addedNodes.length) return;
+	chrome.storage.sync.get(
+		{
+			local_domain: '',
+			web_domain: '',
+		},
+		function (items) {
+			LOCAL_DOMAIN = items.local_domain;
+			WEB_DOMAIN = items.web_domain || LOCAL_DOMAIN;
 
-				const $modalRoot = $mastodon.querySelector('.modal-root');
+			const $initialModalRoot = $mastodon.querySelector('.modal-root');
+			if ($initialModalRoot) {
+				foundModalRoot($initialModalRoot);
+			} else {
+				const observerModalRoot = new MutationObserver(function (mutations_list) {
+					mutations_list.forEach(function (mutation) {
+						if (!mutation.addedNodes.length) return;
 
-				if ($modalRoot) {
-					observerModalRoot.disconnect();
-					foundModalRoot($modalRoot);
-				}
-			});
-		});
+						const $modalRoot = $mastodon.querySelector('.modal-root');
 
-		observerModalRoot.observe($mastodon, { subtree: true, childList: true });
-	}
+						if ($modalRoot) {
+							observerModalRoot.disconnect();
+							foundModalRoot($modalRoot);
+						}
+					});
+				});
+
+				observerModalRoot.observe($mastodon, { subtree: true, childList: true });
+			}
+		}
+	);
 }
 
 function foundModalRoot($modalRoot) {
 	const observer = new MutationObserver(function (mutations_list) {
 		mutations_list.forEach(function (mutation) {
 			if (!mutation.addedNodes.length) return;
+
+			console.log(`Observer found ${mutation.addedNodes.length} new nodes`, mutation.addedNodes);
 
 			// This could be a profile URL or a post URL, depending on the interaction
 			// by the user that made this modal pop up in the first place
@@ -43,64 +58,61 @@ function foundModalRoot($modalRoot) {
 			$choiceBox = $urlInput.closest('.interaction-modal__choices__choice');
 			if (!$choiceBox) return;
 
-			if ($choiceBox.dataset.redirectModificationsState !== 'done') {
-				const $existingParagraph = $choiceBox.querySelector('p');
-				const $existingHeader = $choiceBox.querySelector('h3 span');
+			const $existingParagraph = $choiceBox.querySelector('p');
+			const $existingHeader = $choiceBox.querySelector('h3 span');
 
-				chrome.storage.sync.get(
-					{
-						local_domain: '',
-						web_domain: '',
-					},
-					function (items) {
-						const LOCAL_DOMAIN = items.local_domain;
-						const WEB_DOMAIN = items.web_domain || LOCAL_DOMAIN;
-
-						// Not configured? Show a notification.
-						if (!WEB_DOMAIN && $choiceBox.dataset.redirectModificationsState !== 'unconfigured') {
-							$existingParagraph.innerText = 'Please configure the mastodon-profile-redirect browser extension to more easily follow this account, directly on your Mastodon instance.';
-							$choiceBox.dataset.redirectModificationsState = 'unconfigured';
-							return;
-						}
-
-						// Trim off @domain suffix in case it matches with LOCAL_DOMAIN. This due to https://github.com/mastodon/mastodon/issues/21469
-						if (user.endsWith(`@${LOCAL_DOMAIN}`)) {
-							user = user.substring(0, user.length - `@${LOCAL_DOMAIN}`.length);
-						}
-
-						// Change title to reflect user’s Masto instance
-						$existingHeader.innerText = `On ${LOCAL_DOMAIN}`;
-
-						// Note: in contrast to redirecting based on the window.location, as we do in the background script, instead here we're aiming to redirect based on the interaction just taken.
-						// Did the user just try to follow? Redirect to the profile.
-						// Did the user just try to boost or fav? Redirect to the post in question.
-						// We won't be reading window.location here, because there's no guarantee the user just interacted with the same item the window is navigated to.
-						const urlInputValue = $urlInput.value;
-						const isInteractingWithPost = urlInputValue.match(/[0-9]+$/) !== null;
-
-						// Create view profile/post button
-						const $viewButton = document.createElement('a');
-						$viewButton.classList.add('button', 'button--block');
-						if (isInteractingWithPost) {
-							// User trying to interact with a post, so redirect to the post rather than the author's profile
-							$viewButton.innerText = 'View Post';
-							const encodedTarget = encodeURIComponent(urlInputValue);
-							const newUrl = `https://${WEB_DOMAIN}/authorize_interaction?uri=${encodedTarget}`;
-							$viewButton.href = newUrl;
-						} else {
-							// User trying to interact with profile, so redirect to profile
-							$viewButton.innerText = 'View Profile';
-							$viewButton.href = `https://${WEB_DOMAIN}/@${user}`;
-						}
-
-						// Replace the orig paragraph with the show profile button
-						$existingParagraph.insertAdjacentElement('beforebegin', $viewButton);
-						$choiceBox.removeChild($existingParagraph);
-
-						$choiceBox.dataset.redirectModificationsState = 'done';
-					}
-				);
+			if (!$existingParagraph) {
+				console.error(`$existingParagraph not found`);
 			}
+			if (!$existingHeader) {
+				console.error(`$existingHeader not found`);
+			}
+
+			// Not configured? Show a notification.
+			if (!WEB_DOMAIN) {
+				const newText = 'Please configure the mastodon-profile-redirect browser extension to more easily follow this account, directly on your Mastodon instance.';
+				if ($existingParagraph.innerText !== newText) {
+					$existingParagraph.innerText = newText;
+				}
+				return;
+			}
+
+			// Trim off @domain suffix in case it matches with LOCAL_DOMAIN. This due to https://github.com/mastodon/mastodon/issues/21469
+			if (user.endsWith(`@${LOCAL_DOMAIN}`)) {
+				user = user.substring(0, user.length - `@${LOCAL_DOMAIN}`.length);
+			}
+
+			// Change title to reflect user’s Masto instance
+			const newText = `On ${LOCAL_DOMAIN}`;
+			if ($existingHeader.innerText !== newText) {
+				$existingHeader.innerText = newText;
+			}
+
+			// Note: in contrast to redirecting based on the window.location, as we do in the background script, instead here we're aiming to redirect based on the interaction just taken.
+			// Did the user just try to follow? Redirect to the profile.
+			// Did the user just try to boost or fav? Redirect to the post in question.
+			// We won't be reading window.location here, because there's no guarantee the user just interacted with the same item the window is navigated to.
+			const urlInputValue = $urlInput.value;
+			const isInteractingWithPost = urlInputValue.match(/[0-9]+$/) !== null;
+
+			// Create view profile/post button
+			const $viewButton = document.createElement('a');
+			$viewButton.classList.add('button', 'button--block');
+			if (isInteractingWithPost) {
+				// User trying to interact with a post, so redirect to the post rather than the author's profile
+				$viewButton.innerText = 'View Post';
+				const encodedTarget = encodeURIComponent(urlInputValue);
+				const newUrl = `https://${WEB_DOMAIN}/authorize_interaction?uri=${encodedTarget}`;
+				$viewButton.href = newUrl;
+			} else {
+				// User trying to interact with profile, so redirect to profile
+				$viewButton.innerText = 'View Profile';
+				$viewButton.href = `https://${WEB_DOMAIN}/@${user}`;
+			}
+
+			// Replace the orig paragraph with the show profile button
+			$existingParagraph.insertAdjacentElement('beforebegin', $viewButton);
+			$choiceBox.removeChild($existingParagraph);
 		});
 	});
 
